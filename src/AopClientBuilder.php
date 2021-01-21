83972e0d23a17d232690d475d65e649e58543114
hsu1943 <osnail1943@gmail.com>
2021-01-21 17:43:27 +0800

    加一个builder

---
+++ src/AopClientBuilder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace duckSoft\alipay;
+
+use duckSoft\alipay\aop\AopClient;
+
+/**
+ * Created by PhpStorm.
+ * User: Huan
+ * Date: 2021/1/21
+ * Time: 17:05
+ */
+class AopClientBuilder
+{
+    private $_client = null;
+
+    /**
+     * AopClientBuilder constructor.
+     * @param $config
+     */
+    public function __construct($config)
+    {
+        if (!$this->_client){
+            $aop = new AopClient();
+            foreach ($config as $k => $v) {
+                if (property_exists(AopClient::class, $k)) {
+                    $aop->$k = $v;
+                }
+            }
+            $this->_client = $aop;
+        }
+    }
+
+    public function getAopClient()
+    {
+        return $this->_client;
+    }
+
+}
