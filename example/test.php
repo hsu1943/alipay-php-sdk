e13bdad8ff769109997ed1e84662af7dc8325a5e
hsu1943 <osnail1943@gmail.com>
2021-01-21 17:39:57 +0800

    清理不用文件

--- example/test.php
+++ example/test.php
@@ -5,21 +5,20 @@
  * Date: 2021/1/20
  * Time: 18:42
  */
-
 use duckSoft\alipay\aop\request\AlipayTradeQueryRequest;
-use duckSoft\alipay\model\builder\AlipayTradeQueryContentBuilder;
+use duckSoft\alipay\AopClientBuilder;
 
-require_once './DefaultAlipayClientFactory.php';
-
-$content = new AlipayTradeQueryContentBuilder();
-
-$content->setOutTradeNo($_POST['outTradeNo']);
-$content->setTradeNo($_POST['tradeNo']);
-
+$bizContent = [
+    'out_trade_no' => '系统订单号',
+];
 $request = new AlipayTradeQueryRequest();
-$request->setBizContent ( $content->getBizContent() );
-
-$ext= new DefaultAlipayClientFactory();
-//因为是接口服务，使用exexcute方法获取到返回值
-$response = $ext->aopclientRequestExecute ($request, NULL ,$content->getAppAuthToken());
-echo json_encode($response);
+$request->setBizContent(json_encode($bizContent));
+$config = require __DIR__ . '/AlipayConfig.php';
+try {
+    $aop = (new AopClientBuilder($config))->getAopClient();
+    $response = $aop->execute($request);
+    $responseNode = str_replace(".", "_", $request->getApiMethodName()) . "_response";
+    return $response->$responseNode;
+} catch (\ErrorException $e) {
+    return ['status' => 'error', 'message' => $e->getMessage()];
+}
